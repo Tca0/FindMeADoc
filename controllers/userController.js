c43b63d0a4c57a99a51ea1f727a3293440f35fd0
Taher Khatieb <taher.khtieb@gmail.com>
2022-03-23 19:49:28 +0000

    Sending mails functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,8 @@ import User from "../models/user.js";
 import Patient from "../models/patient.js";
 import Doctor from "../models/doctor.js";
 import { validationResult } from "express-validator";
-import passwordsFunctions from "../db/helpers/passwordsFunctions.js"
+import passwordsFunctions from "../db/helpers/passwordsFunctions.js";
+import sendConfirmationEmail from "../db/helpers/mailer.js";
 // get all users
 async function getUsersList(req, res, next) {
   try {
@@ -24,13 +25,18 @@ async function register(req, res, next) {
     // then they need to re-activate their accounts again
     if (existedUser && existedUser.active === 0) throw new Error("not active");
     if (existedUser) throw new Error("user existed");
-    if (!passwordsFunctions.confirmPassword(req.body.password,req.body.confirmPassword)) {
-      throw new Error("password not confirmed")
+    if (
+      !passwordsFunctions.confirmPassword(
+        req.body.password,
+        req.body.confirmPassword
+      )
+    ) {
+      throw new Error("password not confirmed");
     }
     const newUser = {
       email: req.body.email,
       password: req.body.password,
-      role: req.body.role
+      role: req.body.role,
     };
     console.log(newUser);
     //hashing password
@@ -44,7 +50,7 @@ async function register(req, res, next) {
       activationCode: code,
       //active account will be after verification process
     });
-    console.log(createdUser.role)
+    console.log(createdUser.role);
     //According to the role add user to the right schema
     if (createdUser.role === "patient") {
       const newPatient = await Patient.create({
@@ -58,6 +64,9 @@ async function register(req, res, next) {
         //active account will be after verification process
       });
     }
+    //call send email function
+    console.log("calling send mail function");
+    await sendConfirmationEmail(createdUser.email, code);
     res.status(200).json({
       message:
         "registration successful, login please to complete your registration to activate your account.",
@@ -70,7 +79,7 @@ async function register(req, res, next) {
 //login process and generating a token
 async function login(req, res, next) {
   try {
-      //handle email format error
+    //handle email format error
     const errors = validationResult(req);
     console.log(errors.errors);
     if (errors.errors.length !== 0) throw new Error("invalid email format");
@@ -91,7 +100,7 @@ async function login(req, res, next) {
       req.body.password
     );
     console.log("user controller", isItMatch);
-    if(!isItMatch) throw new Error("invalid login");
+    if (!isItMatch) throw new Error("invalid login");
 
     //creating a variable to cary logged in user (necessary info for user)
     const payload = {
@@ -107,24 +116,27 @@ async function login(req, res, next) {
 }
 //when users complete their profiles then account will assigned as completed
 async function verifyAccount(req, res, next) {
-  const { email, code } = req.body
-  console.log(email, code)
+  const { email, code } = req.body;
+  console.log(email, code);
   //user will attach the code with their email then we check if match then user account will be activated
-    try{
-      const user = await User.findOne({
-        email: email
-      })
-      // console.log(user)
-      console.log(user.activationCode)
-      if(!user || user.activationCode !== code) throw new Error("invalid activation");
-      //user existed and code is right ==> we need to set account as active and save data
-      user.activationCode = null
-      user.active = 1
-      await user.save()
-      res.status(200).json({ message: "Account activated, login please to complete your information"})
-    } catch(err) {
-      next(err)
-    }
+  try {
+    const user = await User.findOne({
+      email: email,
+    });
+    // console.log(user)
+    console.log(user.activationCode);
+    if (!user || user.activationCode !== code)
+      throw new Error("invalid activation");
+    //user existed and code is right ==> we need to set account as active and save data
+    user.activationCode = null;
+    user.active = 1;
+    await user.save();
+    res.status(200).json({
+      message: "Account activated, login please to complete your information",
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 async function changePassword(req, res, next) {
   // current user will give access to user schema
@@ -133,31 +145,31 @@ async function changePassword(req, res, next) {
   // first this function will match the old password with the old one that stored
   // if it's match then it will check if new password and confirmPassword match
   //if it's then will set the new password after hashing it.
-  const { oldPassword, newPassword, confirmPassword} = req.body
-  try{
+  const { oldPassword, newPassword, confirmPassword } = req.body;
+  try {
     const isItMatch = await passwordsFunctions.comparePassword(
-    req.currentUser.password,
-    oldPassword
-  );
-  if (!isItMatch) throw new Error("invalid old password");
-  if (!passwordsFunctions.confirmPassword(newPassword, confirmPassword)) {
-    throw new Error("password not confirmed");
+      req.currentUser.password,
+      oldPassword
+    );
+    if (!isItMatch) throw new Error("invalid old password");
+    if (!passwordsFunctions.confirmPassword(newPassword, confirmPassword)) {
+      throw new Error("password not confirmed");
+    }
+    const password = await passwordsFunctions.hashPassword(newPassword);
+    const newUser = await User.findOneAndUpdate(
+      { email: req.currentUser.email },
+      { password: password },
+      { new: true }
+    );
+    res.status(200).json({ message: "password has been changed" });
+  } catch (err) {
+    next(err);
   }
-  const password = await passwordsFunctions.hashPassword(newPassword);
-  const newUser = await User.findOneAndUpdate({ email: req.currentUser.email },
-    { password: password },
-    { new: true})
-    res.status(200).json({ message: "password has been changed"})
-  }
-  catch(err) {
-    next(err)
-  }
-
 }
 export default {
   getUsersList,
   register,
   login,
   verifyAccount,
-  changePassword
+  changePassword,
 };
